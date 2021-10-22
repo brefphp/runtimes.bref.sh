b0a7faad7554405e5d9e78babf5b51bad4df6e7d
Sebastian Grodzicki <sebastian@grodzicki.pl>
2021-10-22 18:07:19 +0200

    Add Bahrain, Hong Kong, and Osaka regions

--- index.php
+++ index.php
@@ -30,12 +30,15 @@ $regions = [
     'us-east-2',
     'us-west-1',
     'us-west-2',
+    'ap-east-1',
     'af-south-1',
     'ap-south-1',
     'ap-northeast-1',
     'ap-northeast-2',
+    'ap-northeast-3',
     'ap-southeast-1',
     'ap-southeast-2',
+    'me-south-1',
 ];
 
 $app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) use ($regions) {
